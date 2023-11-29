54b8b17ee3b72a617a2c704c94f56e6c252b25d5
Idris Adebowale <idrys01@gmail.com>
2023-11-29 16:27:28 +0000

    add error message to rejected promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,5 +4,6 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([uploadPhoto(fileName),
     signUpUser(firstName, lastName)])
-    .then((resolve) => resolve.map(({ status, value }) => ({ status, value })));
+    .then((resolve) => resolve.map(({ status, value, reason }) => (
+      { status, value: value || reason.message })));
 }
