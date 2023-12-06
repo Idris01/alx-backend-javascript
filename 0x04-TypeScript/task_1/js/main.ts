121ef85ad4a17f06b1888857e6faf1be8cf27c4d
Idris Adebowale <idrys01@gmail.com>
2023-12-06 17:20:11 +0000

    StudentClass class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,31 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
   }
+  
+interface printTeacherFunction {
+  firstName: string;
+  lastName: string;
+  }
+ 
+export function printTeacher(firstName:string, lastName:string):string {
+  return `${firstName[0]}. ${lastName}`;
+  }
+
+  
+export class StudentClass{
+  firstName:string;
+  lastName:string;
+
+  constructor(firstName:string, lastName:string) {
+    this.firstName=firstName;
+    this.lastName=lastName;
+  }
+
+  workOnHomework():string {
+    return 'Currently working';
+  }
+
+  displayName():string {
+    return this.firstName;
+  }
+}
