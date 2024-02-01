d86698e05cfba5ce5f6ada8e0ecb7be21a870280
Idris Adebowale <idrys01@gmail.com>
2024-02-01 10:30:59 +0000

    fix: collect all test inside a single describe

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,26 +1,20 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
-describe('add 1 and 2', function() {
-  it('checks equality', function() {
+describe('calculateNumber', function() {
+  it('1 plus 3 equals 4', function() {
     assert.equal(calculateNumber(1, 3), 4);
     });
-});
 
-describe('add 1 and 3.7', function() {
-  it('checks equality', function() {
+  it('1 plus 3.7 equals 5', function() {
     assert.equal(calculateNumber(1, 3.7), 5);
     });
-});
 
-describe('add 1.2 and 3.7', function() {
-  it('checks equality', function() {
+  it('1.2 plus 3.7 equals 5', function() {
     assert.equal(calculateNumber(1.2, 3.7), 5);
     });
-});
 
-describe('add 1.5 and 3.7', function() {
-  it('checks equality', function() {
+  it('1.5 plus 3.7 equals 6', function() {
     assert.equal(calculateNumber(1.5, 3.7), 6);
     });
 });
