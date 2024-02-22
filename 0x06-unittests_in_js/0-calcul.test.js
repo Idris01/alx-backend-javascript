ba96406c8b4328aa4361e514e415b6a38cb4616a
Idris Adebowale <idrys01@gmail.com>
2024-02-22 17:25:59 +0000

    add suite for second number

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,27 +2,21 @@ const calculateNumber = require('./0-calcul');
 const assert = require('assert');
 
 describe('calculateNumber', () => {
-  it('1 + 3 equals 4', () => {
-    assert.equal(calculateNumber(1, 3), 4);
-    assert.equal(calculateNumber(0, 3), 3);
+  it('round first number', () => {
     assert.equal(calculateNumber(1, 0), 1);
-  });
-
-  it('1 + 3.7 equals 5', () => {
-    assert.equal(calculateNumber(1, 3.7), 5);
-    assert.equal(calculateNumber(1, 0), 1);
-    assert.equal(calculateNumber(0, 3.7), 4);
-  });
-
-  it('1.2 + 3.7 equals 5', () => {
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-    assert.equal(calculateNumber(0, 3.7), 4);
+    assert.equal(calculateNumber(1.5, 0), 2);
     assert.equal(calculateNumber(1.2, 0), 1);
   });
 
-  it('1.5 + 3.7 equals 6', () => {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
-    assert.equal(calculateNumber(1.5, 0), 2);
+  it('round second number', () => {
+    assert.equal(calculateNumber(0, 3), 3);
     assert.equal(calculateNumber(0, 3.7), 4);
+  });
+
+  it('return required result', () => {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+    assert.equal(calculateNumber(1, 3), 4);
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+    assert.equal(calculateNumber(1, 3.7), 5);
   });
 });
