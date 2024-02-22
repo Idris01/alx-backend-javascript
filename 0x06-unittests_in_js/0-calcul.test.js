c020c241d673bc88812389ca5fdf71e27d2499aa
Idris Adebowale <idrys01@gmail.com>
2024-02-22 17:07:06 +0000

    add checks for each number

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,25 +1,28 @@
+const calculateNumber = require('./0-calcul');
 const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
 
-describe('calculateNumber', function() {
-  it('1 plus 3 equals 4', function() {
+describe('calculateNumber', () => {
+  it('1 + 3 equals 4', () => {
     assert.equal(calculateNumber(1, 3), 4);
-    });
+    assert.equal(calculateNumber(0, 3), 3);
+    assert.equal(calculateNumber(1, 0), 1);
+  });
 
-  it('1 plus 3.7 equals 5', function() {
+  it('1 + 3.7 equals 5', () => {
     assert.equal(calculateNumber(1, 3.7), 5);
-    });
+    assert.equal(calculateNumber(1, 0), 1);
+    assert.equal(calculateNumber(0, 3.7), 4);
+  });
 
-  it('1.2 plus 3.7 equals 5', function() {
+  it('1.2 + 3.7 equals 5', () => {
     assert.equal(calculateNumber(1.2, 3.7), 5);
-    });
+    assert.equal(calculateNumber(0, 3.7), 4);
+    assert.equal(calculateNumber(1.2, 0), 1);
+  });
 
-  it('1.5 plus 3.7 equals 6', function() {
+  it('1.5 + 3.7 equals 6', () => {
     assert.equal(calculateNumber(1.5, 3.7), 6);
-    });
-  
-  it('-1.5 plus 3.7 equals 3', function() {
-    assert.equal(calculateNumber(-1.5, 3.7), 3);
-    });
-
+    assert.equal(calculateNumber(1.5, 0), 2);
+    assert.equal(calculateNumber(0, 3.7), 4);
+  });
 });
